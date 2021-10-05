d2439c29d98ac43f6c795f425622132f4c17dd0e
alejandro toledo <atoledofr@gmail.com>
2021-10-05 18:49:50 -0500

    add another way of creating the server without express

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+var fs = require('fs');
+const url = require('url');
+
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  // res.statusCode = 200
+  // res.writeHead(200, {'Content-Type': 'text/plain'})
+  // const page = url.parse(req.url).pathname DEPRECATED
+  
+  if (req.url === '/'){
+    fs.readFile('index.html', (err, data) => {
+      if (err) throw err;
+      res.writeHeader(200, {'Content-Type': 'text-html'})
+      res.write(data);
+      return res.end();
+    });
+  } else if (req.url === '/about'){
+    fs.readFile('about.html', (err, data) => {
+      if (err) throw err;
+      res.writeHeader(200, {'Content-Type': 'text-html'})
+      res.write(data);
+      return res.end();
+    });
+    
+  } else if (req.url === '/contact'){
+    fs.readFile('contact-me.html', (err, data) => {
+      if (err) {throw err};
+      res.writeHeader(200, {'Content-Type': 'text-html'})
+      res.write(data);
+      return res.end();
+    });
+    
+  } else {
+    fs.readFile('404.html', (err, data) => {
+      if (err) throw err;
+      res.writeHeader(404, {'Content-Type': 'text-html'})
+      res.write(data);
+      return res.end
+    });
+    
+  }
+})
+
+server.listen(port, () => {
+  console.log('listening on port: ' + port)
+});
